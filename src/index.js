e572c70a902167584ff29ca7cbc1ad8f16feb421
Toleek <tom.0.0.7@yandex.ru>
2021-08-09 22:04:41 +0300

    Task typical arrays problem complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || !array.length) return 0;
+  
+  let min = 0;
+  
+  for(key in array)
+  {
+	  if(min > array[key])
+	  {
+		  min = array[key];
+	  }
+  }
+  
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || !array.length) return 0;
+  
+  let max = 0;
+  
+  for(key in array)
+  {
+	  if(max < array[key])
+	  {
+		  max = array[key];
+	  }
+  }
+  
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || !array.length) return 0;
+  
+  let avg = 0;
+  
+  for(key in array)
+  {
+	avg += array[key];
+  }
+  
+  return avg / array.length;
 }
